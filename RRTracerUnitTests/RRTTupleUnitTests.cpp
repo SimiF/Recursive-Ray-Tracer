f64127bad9a1b3673828d246e99ae3c06e89cf53
Simeon Florea <simeon.florea@oit.edu>
2022-09-04 11:41:53 -0700

    initial commit of testing framework

---
+++ RRTracerUnitTests/RRTTupleUnitTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RRTTupleUnitTests
+{
+	TEST_CLASS(RRTTupleUnitTests)
+	{
+	public:
+
+		TEST_METHOD(BLANK)
+		{
+		}
+	};
+}
